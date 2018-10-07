1f7a7e6f37d2ec47c4b4613bf0cee71ed9c7c550
Wydman <baptiste.int@gmail.com>
2018-10-07 02:07:57 +0200

    TaskManager First Commit

---
+++ Assets/Script/IAMouseScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAMouseScript : MonoBehaviour
+{
+
+
+    [SerializeField] private float spawnLerpDelay;
+
+    public TaskManagerScript taskManagerScript;
+
+    private Transform _lerpTarget1;
+    private Transform _lerpTarget2;
+    private Transform _focusTarget;
+
+    private bool _firstTargetSelected = false;
+    private bool _canLerp = false;
+
+    private void Awake()
+    {
+        _lerpTarget1 = taskManagerScript.SelectProcessTarget;
+        _lerpTarget2 = taskManagerScript.SelectDeleteTarget;
+        _focusTarget = _lerpTarget1;
+
+        StartCoroutine(InstantiationLerpDelay());
+    }
+
+    private void Update()
+    {
+        taskManagerScript.IsTargetObstructed(_focusTarget);
+        ChooseTarget();
+        if (_canLerp)
+            LerpToTarget();
+    }
+
+    private void ChooseTarget()
+    {
+        if (!_firstTargetSelected)
+            _focusTarget = _lerpTarget1;
+        else
+            _focusTarget = _lerpTarget2;
+
+        if (transform.position == _lerpTarget1.position)
+            _firstTargetSelected = true;
+
+    }
+
+    private void LerpToTarget()
+    {
+        transform.position = new Vector3
+            (Mathf.Lerp(transform.position.x, _focusTarget.position.x, 0.05f),
+            Mathf.Lerp(transform.position.y, _focusTarget.position.y, 0.05f),
+            0);
+    }
+
+    IEnumerator InstantiationLerpDelay()
+    {
+        yield return new WaitForSeconds(spawnLerpDelay);
+        _canLerp = true;
+    }
+}
---
+++ Assets/Script/TaskManagerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManagerScript : MonoBehaviour {
+
+    public Transform SelectProcessTarget;
+    public Transform SelectDeleteTarget;
+
+    [SerializeField] private float timeBeforeRelocate;
+    [SerializeField] private Camera playerCamera;
+
+    private bool _targetIsObstructed = false;
+    private bool _isRelocateCouroutineStarted = false;
+    private Coroutine _obstructedTimerCoroutine;
+
+    private void Awake()
+    {
+        SelectProcessTarget = SelectProcessTarget.transform;
+    }
+
+    public void IsTargetObstructed(Transform targetPosition)
+    {
+        var cameraTransform = playerCamera.transform.position;
+        int layer_mask = LayerMask.GetMask("Ads");
+
+        if (Physics.Linecast(cameraTransform, targetPosition.position, layer_mask))
+        {
+            _targetIsObstructed = true;
+            if (!_isRelocateCouroutineStarted)
+            {
+                _obstructedTimerCoroutine = StartCoroutine(TimeObstructedBeforeRelocate());
+                _isRelocateCouroutineStarted = true;
+            }
+        }
+        else
+        {
+            if (_isRelocateCouroutineStarted)
+            {
+                StopCoroutine(_obstructedTimerCoroutine);
+                _isRelocateCouroutineStarted = false;
+            }
+            _targetIsObstructed = false;
+        }
+    }
+
+    public void RelocateTaskManager()
+    {
+        transform.position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
+        _isRelocateCouroutineStarted = false;
+    }
+
+    IEnumerator TimeObstructedBeforeRelocate()
+    {
+        yield return new WaitForSeconds(timeBeforeRelocate);
+        RelocateTaskManager();
+    }
+}
