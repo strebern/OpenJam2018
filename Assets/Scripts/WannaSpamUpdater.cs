4bcbc407369b36cbc4f4c878b033a3224f2198f8
SimonT <simon.trebern@outlook.com>
2018-10-08 23:18:24 +0200

    Added WannaSpam Panel

---
+++ Assets/Scripts/WannaSpamUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class WannaSpamUpdater : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro _countdownText;
+    [SerializeField] private TextMeshPro _scoreText;
+    private float _timer = 0f;
+    private bool _countdownStarted = false;
+
+    private void Update()
+    {
+        if (_countdownStarted)
+            _countdownText.text += "" + TimeSpan.FromSeconds(_timer);
+    }
+
+    // PUBLIC
+
+    public void StartCountdown()
+    {
+        _countdownStarted = true;
+    }
+
+    public void UpdateScore(int score)
+    {
+        _scoreText.text = "" + score;
+    }
+}
