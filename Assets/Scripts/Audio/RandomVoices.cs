aced413385590e647dc392b428b498aaca412f2d
SimonT <simon.trebern@outlook.com>
2018-10-09 01:41:50 +0200

    Added musics

--- Assets/Scripts/Audio/RandomVoices.cs
+++ Assets/Scripts/Audio/RandomVoices.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class RandomVoices : MonoBehaviour
 {
     [SerializeField] private AudioClip[] clips;
-    [SerializeField] private float SecondsBetweenIncrement = 30f;
-    [SerializeField] private float SecondsBetweenSounds = 10f;
+    [SerializeField] private float SecondsBetweenIncrement = 20f;
+    [SerializeField] private float SecondsBetweenSounds = 8f;
 
     private AudioSource _audioSource;
     private int index = 0;
@@ -18,23 +18,28 @@ public class RandomVoices : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(PlaySoundsRoutine());
         StartCoroutine(IncrementIndex());
     }
 
-    private void PlayRandomAtIndex()
+    // PUBLIC
+
+    public void PlayRandomAtIndex()
     {
         if (_canPlaySound)
         {
-            int indexToPlay = Mathf.Clamp(index + Random.Range(-1, 1), 0, clips.Length - 1);
+            int random = index + Random.Range(-2, 3);
+            int indexToPlay = Mathf.Clamp(random, 0, clips.Length - 1);
             _audioSource.clip = clips[indexToPlay];
             _audioSource.Play();
+            _canPlaySound = false;
+            StartCoroutine(PlaySoundsRoutine());
         }
     }
 
+    // PRIVATE
+
     private IEnumerator PlaySoundsRoutine()
     {
-        _canPlaySound = false;
         yield return new WaitForSeconds(SecondsBetweenSounds);
         _canPlaySound = true;
     }
